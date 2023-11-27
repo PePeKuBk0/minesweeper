7bf633ac0e7e06c1182f13d81f423c041e7235da
PePeKuBk0 <jakub.venetianer@gmail.com>
2023-11-27 12:33:17 +0100

    new class Pepega

---
+++ Minesweeper/Pepega.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class Pepega here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+public class Pepega {
+    private Main main;
+    public Pepega() {
+        // initialise instance variables
+    }
+}
