fc80e699a86878031652e530935f398920a33cca
Enerek27 <tomhroncek@gmail.com>
2023-12-11 12:31:14 +0100

    test 1

---
+++ Minesweeper/App.java
@@ -0,0 +1,12 @@
+
+/**
+ * Write a description of class App here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class App {
+    public static void main(String[] args) throws Exception{
+        Minesweeper minesweeper = new Minesweeper();
+    }
+}
